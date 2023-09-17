4b1c75e83eda9aad374f4a11e45c5e9dd20f1458
matthewbarnes1 <matthew.barnes35@gmail.com>
2023-09-17 19:17:16 -0400

    routing fixed for express

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const app = express();
 const PORT = 3001;
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/pages/index.html');
+});
 
-app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+app.get ('/notes', (req, res) => {
+  res.sendFile(__dirname + '/public/pages/notes.html');
+});
 
-app.get('/send', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/sendFile.html'))
-);
-
-app.get('/routes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/routes.html'))
-);
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
   );
+
+
